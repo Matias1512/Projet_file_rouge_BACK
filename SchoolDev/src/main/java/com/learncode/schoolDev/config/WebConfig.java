d20a78470f0b61647ab2c319ab207049d486d3ed
Matias1512 <matias.caturla@gmail.com>
2025-04-01 15:52:00 +0200

    CORS 3

--- SchoolDev/src/main/java/com/learncode/schoolDev/config/WebConfig.java
+++ SchoolDev/src/main/java/com/learncode/schoolDev/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.learncode.schoolDev.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,10 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://schooldev.duckdns.org") // Ton frontend en dev et prod
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+         registry.addMapping("/**")
+                 .allowedMethods(CorsConfiguration.ALL)
+                 .allowedHeaders(CorsConfiguration.ALL)
+                 .allowedOriginPatterns(CorsConfiguration.ALL);
     }
 }
