78b1d5e58d33de9814303f928ce6a9eef6d5e1c0
Matias1512 <matias.caturla@gmail.com>
2025-04-01 15:59:42 +0200

    CORS 4

--- SchoolDev/src/main/java/com/learncode/schoolDev/config/WebConfig.java
+++ SchoolDev/src/main/java/com/learncode/schoolDev/config/WebConfig.java
@@ -1,18 +1,27 @@
 package com.learncode.schoolDev.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-         registry.addMapping("/**")
-                 .allowedMethods(CorsConfiguration.ALL)
-                 .allowedHeaders(CorsConfiguration.ALL)
-                 .allowedOriginPatterns(CorsConfiguration.ALL);
+@Component
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedMethods(CorsConfiguration.ALL)
+                        .allowedHeaders(CorsConfiguration.ALL)
+                        .allowedOriginPatterns(CorsConfiguration.ALL);
+            }
+        };
     }
 }
