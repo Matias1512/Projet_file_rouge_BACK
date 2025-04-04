3ce55b1e84ab964c440cb6a2be22e2b2281c6db0
Matias1512 <matias.caturla@gmail.com>
2025-04-04 13:28:28 +0200

    web config for cors

--- SchoolDev/src/main/java/com/learncode/schoolDev/config/WebConfig.java
+++ SchoolDev/src/main/java/com/learncode/schoolDev/config/WebConfig.java
@@ -3,25 +3,38 @@ package com.learncode.schoolDev.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Component
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+    /**
+     * CORS configuration to allow all origins, methods, and headers.
+     * This is useful for development purposes but should be restricted in production.
+     */
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowedOrigins("*");
+    }
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
 
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry
-                        .addMapping("/**")
-                        .allowedMethods(CorsConfiguration.ALL)
-                        .allowedHeaders(CorsConfiguration.ALL)
-                        .allowedOriginPatterns(CorsConfiguration.ALL);
-            }
-        };
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
