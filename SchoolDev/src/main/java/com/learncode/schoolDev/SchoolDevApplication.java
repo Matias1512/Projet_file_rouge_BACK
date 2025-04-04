41b16e9e2867cf75702dd2c4cce797f5cfb5437a
Matias1512 <matias.caturla@gmail.com>
2025-04-04 14:27:08 +0200

    try again

--- SchoolDev/src/main/java/com/learncode/schoolDev/SchoolDevApplication.java
+++ SchoolDev/src/main/java/com/learncode/schoolDev/SchoolDevApplication.java
@@ -2,12 +2,14 @@ package com.learncode.schoolDev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
+@Configuration
 @OpenAPIDefinition(
     info = @Info(
         title = "API Documentation",
