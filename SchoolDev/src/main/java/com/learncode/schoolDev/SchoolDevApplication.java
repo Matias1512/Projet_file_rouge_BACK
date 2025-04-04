4c56eb70aa3243da718a5172aebec97a694f8991
Matias1512 <matias.caturla@gmail.com>
2025-04-04 14:01:59 +0200

    forcer le passage vers https et non http

--- SchoolDev/src/main/java/com/learncode/schoolDev/SchoolDevApplication.java
+++ SchoolDev/src/main/java/com/learncode/schoolDev/SchoolDevApplication.java
@@ -3,7 +3,21 @@ package com.learncode.schoolDev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title = "API Documentation",
+        version = "v1"
+    ),
+    servers = @Server(
+        url = "https://schooldev.duckdns.org",
+        description = "Serveur principal"
+    )
+)
 public class SchoolDevApplication {
 
 	public static void main(String[] args) {
